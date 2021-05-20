776e9e449aa43f70a4cf79be535e130f6508a09e
AngeloBenavidez <benavidezangelo9@gmail.com>
2021-05-20 09:57:13 -0500

    Arreglando estilos de Header y Footer

--- componentes/Footer.js
+++ componentes/Footer.js
@@ -24,68 +24,27 @@ const Footer = () => {
   ];
 
   return (
-    <section className='footer section'>
+    <footer className='footer'>
       <Container>
         <Row>
-          <Col lg={4}>
+          <Col lg={12}>
             <div className='mb-4'>
               <p className='text-muted mt-4 mb-2'>abenavidez.dev@gmail.com</p>
               <h6 className='text-muted font-weight-normal'>+593 985386708</h6>
             </div>
-          </Col>
-          <Col lg={8}>
-            <Row>
-              {links.map((link, key) => (
-                <Col key={key} md={4}>
-                  <h6 className='text-dark mb-3'>{link.title}</h6>
-                  <ul className='list-unstyled company-sub-menu'>
-                    {link.child.map((fLink, key) => (
-                      <li key={key}>
-                        <a href={fLink.link}>{fLink.title}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </Col>
-              ))}
-
-              <Col md={4}>
-                <h6 className='text-dark mb-3'>Our Address</h6>
-                <p className='text-muted f-14'>
-                  1234 Lorem Ipsum dummy text, 12345
-                </p>
-                <h6 className='text-muted pb-2'>Email: email@email.com</h6>
-                <ul className='list-unstyled footer-social-list mt-4'>
-                  <li className='list-inline-item'>
-                    <a href='#'>
-                      <i className='mdi mdi-facebook'></i>
-                    </a>
-                  </li>
-                  <li className='list-inline-item'>
-                    <a href='#'>
-                      <i className='mdi mdi-instagram'></i>
-                    </a>
-                  </li>
-                  <li className='list-inline-item'>
-                    <a href='#'>
-                      <i className='mdi mdi-linkedin'></i>
-                    </a>
-                  </li>
-                </ul>
-              </Col>
-            </Row>
           </Col>
         </Row>
         <Row className='mt-5'>
           <Col md={12}>
             <div className='text-center text-muted'>
               <p className='mb-0 f-15'>
-                2021 © Tickets PA. Design with Love 💙 by Angelo Benavidez
+                2021 © Tickets PA. Design with Love 🤍 by Angelo Benavidez
               </p>
             </div>
           </Col>
         </Row>
       </Container>
-    </section>
+    </footer>
   );
 };
 export default Footer;
