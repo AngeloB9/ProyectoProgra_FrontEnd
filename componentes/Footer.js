9bfff37734d6e9fc060b8c4bd7b442691582ccff
AngeloBenavidez <benavidezangelo9@gmail.com>
2021-06-01 21:57:46 -0500

    Agrego Footer y Estilos de Footer

---
+++ componentes/Footer.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import SchoolIcon from '@material-ui/icons/School';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+//Versión 1 del navbar
+class Prueba extends Component {
+  render() {
+    return (
+      <div className='contenedorBarrafootF'>
+        <div className='barrafootF'>
+          <table className='footIconTableF'>
+            <tbody>
+              <tr>
+                <td className='footiconF'>
+                  <a
+                    className='footLink text-reset text-decoration-none'
+                    target='_blank'
+                    href='https://www.facebook.com'
+                    rel='noopener noreferrer'>
+                    <FacebookIcon style={{ fontSize: 45 }} />
+                  </a>
+                </td>
+                <td className='footiconF'>
+                  <a
+                    className='footLink text-reset text-decoration-none'
+                    target='_blank'
+                    href='https://www.twitter.com'
+                    rel='noopener noreferrer'>
+                    <TwitterIcon style={{ fontSize: 45 }} />
+                  </a>
+                </td>
+                <td className='footiconF'>
+                  <a
+                    className='footLink text-reset text-decoration-none'
+                    target='_blank'
+                    href='https://www.youtube.com'
+                    rel='noopener noreferrer'>
+                    <YouTubeIcon style={{ fontSize: 45 }} />
+                  </a>
+                </td>
+                <td className='footiconF'>
+                  <a
+                    className='footLink text-reset text-decoration-none'
+                    target='_blank'
+                    href='https://www.instagram.com'
+                    rel='noopener noreferrer'>
+                    <InstagramIcon style={{ fontSize: 45 }} />
+                  </a>
+                </td>
+                <td className='footiconF'>
+                  <a
+                    className='footLink text-reset text-decoration-none'
+                    target='_blank'
+                    href='https://www.linkedin.com'
+                    rel='noopener noreferrer'></a>
+                  <LinkedInIcon style={{ fontSize: 45 }} />
+                </td>
+                <td className='footiconF'>
+                  <a
+                    className='footLink text-reset text-decoration-none'
+                    target='_blank'
+                    href='https://www.puce.edu.ec'
+                    rel='noopener noreferrer'>
+                    <SchoolIcon style={{ fontSize: 45 }} />
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className='footLinea1F'>
+            <table className='footItemTableF'>
+              <tbody>
+                <tr>
+                  <td className='footitemF'>
+                    <ul className='footLinkList'>
+                      <li>
+                        <h6 className='footTitle'>Github</h6>
+                      </li>
+                      <li>
+                        <a
+                          className='footLink'
+                          href='https://github.com/AngeloB9/ProyectoProgra_FrontEnd'
+                          target='_blank'
+                          rel='noopener noreferrer'>
+                          Frontend
+                        </a>
+                      </li>
+
+                      <li>
+                        <a
+                          className='footLink'
+                          href=''
+                          target='_blank'
+                          rel='noopener noreferrer'>
+                          Backend - Proximamente
+                        </a>
+                      </li>
+                    </ul>
+                  </td>
+                  <td className='footitemF'>
+                    <ul className='footLinkList'>
+                      <li>
+                        <h6 className='footTitle'>Tecnologías</h6>
+                      </li>
+                      <li>
+                        <a
+                          className='footLink'
+                          href='https://nextjs.org/docs'
+                          target='_blank'
+                          rel='noopener noreferrer'>
+                          Next Js
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          className='footLink'
+                          href='https://docs.microsoft.com/en-us/dotnet/framework/'
+                          target='_blank'
+                          rel='noopener noreferrer'>
+                          .NetFramework
+                        </a>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='HuellaPersonal'>
+            Made with Love 🤍 by Angelo Benavidez
+          </div>
+          <div className='footInfo'>
+            <table className='footInfoTable'>
+              <tbody>
+                <tr>
+                  <td>2021 © Tickets PA.</td>
+                  <td className='infoDerecha'>Whatsapp: +593 99 50 83 003</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Prueba;
